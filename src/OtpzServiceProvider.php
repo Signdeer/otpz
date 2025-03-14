562ed6a39fba687d4c6912bf0bb91cb5a7d5c607
Ben Bjurstrom <benbjurstrom@users.noreply.github.com>
2025-03-14 10:21:54 -0700

    19 integrate with laravel starter kits (#20)

--- src/OtpzServiceProvider.php
+++ src/OtpzServiceProvider.php
@@ -2,11 +2,6 @@
 
 namespace BenBjurstrom\Otpz;
 
-use BenBjurstrom\Otpz\Http\Controllers\GetOtpController;
-use BenBjurstrom\Otpz\Http\Controllers\PostOtpController;
-use BenBjurstrom\Otpz\Mail\OtpzMail;
-use BenBjurstrom\Otpz\Models\Otp;
-use Illuminate\Support\Facades\Route;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -24,28 +19,5 @@ class OtpzServiceProvider extends PackageServiceProvider
             ->hasConfigFile()
             ->hasViews('otpz')
             ->hasMigration('create_otps_table');
-
-        $this->registerOtpzRouteMacro();
-    }
-
-    protected function registerOtpzRouteMacro(): self
-    {
-        Route::macro('otpRoutes', function () {
-            Route::get('otpz/{id}', GetOtpController::class)
-                ->name('otp.show')->middleware('guest');
-
-            Route::post('otpz/{id}', PostOtpController::class)
-                ->name('otp.post')->middleware('guest');
-
-            if (app()->environment('local')) { // Only for local environment
-                Route::get('/otpz', function () {
-                    $otp = Otp::find(1);
-
-                    return new OtpzMail($otp, '12345689');
-                });
-            }
-        });
-
-        return $this;
     }
 }
